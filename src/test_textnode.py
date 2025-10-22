6534e631d8ed365b13d56ab09708636727b79509
limitedink <54423958+limitedink@users.noreply.github.com>
2025-10-22 20:17:32 +1100

    add tests for TextNode equality and inequality

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -9,6 +9,26 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
 
+    def test_eq2(self):
+        node = TextNode("", TextType.LINK)
+        node2 = TextNode("", TextType.LINK)
+        self.assertEqual(node, node2)
+
+    def test_noeq(self):
+        node = TextNode("text", TextType.PLAIN, "www.url.com")
+        node2 = TextNode("text", TextType.PLAIN, "www.url2.com")
+        self.assertNotEqual(node, node2)
+
+    def test_noeq2(self):
+        node = TextNode("italics", TextType.ITALIC, None)
+        node2 = TextNode("italics", TextType.ITALIC, "www.url.com")
+        self.assertNotEqual(node, node2)
+
+    def test_noeq3(self):
+        node = TextNode("same", TextType.PLAIN)
+        node2 = TextNode("same", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
 
 if __name__ == "__main__":
     unittest.main()
