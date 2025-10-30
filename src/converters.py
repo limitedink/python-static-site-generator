25c46786789b791ce4b42ad65c6d94a28743bbab
limitedink <54423958+limitedink@users.noreply.github.com>
2025-10-30 16:45:41 +1100

    implement text_node_to_html_node converter function

--- src/converters.py
+++ src/converters.py
@@ -1,1 +1,19 @@
-def text_node_to_html_node(text_node):
+from textnode import TextType
+from htmlnode import LeafNode
+
+
+def text_node_to_html_node(node):
+    if node.text_type == TextType.TEXT:
+        return LeafNode(None, node.text)
+    elif node.text_type == TextType.BOLD:
+        return LeafNode("b", node.text)
+    elif node.text_type == TextType.ITALIC:
+        return LeafNode("i", node.text)
+    elif node.text_type == TextType.CODE:
+        return LeafNode("code", node.text)
+    elif node.text_type == TextType.LINK:
+        return LeafNode("a", node.text, {"href": node.url})
+    elif node.text_type == TextType.IMAGE:
+        return LeafNode("img", "", {"src": node.url, "alt": node.text})
+    else:
+        raise Exception(f"unsupported TextType: {node.text_type}")
