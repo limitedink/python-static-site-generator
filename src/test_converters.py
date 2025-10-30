6f939f1ca22bffc0c1122ec81a0a4d86130ff0c4
limitedink <54423958+limitedink@users.noreply.github.com>
2025-10-30 17:02:05 +1100

    add comprehensive tests for text_node_to_html_node converter

--- src/test_converters.py
+++ src/test_converters.py
@@ -11,6 +11,49 @@ class TestConverters(unittest.TestCase):
         self.assertEqual(html_node.tag, None)
         self.assertEqual(html_node.value, "This is a text node")
 
+    def test_bold(self):
+        node = TextNode("Strong", TextType.BOLD)
+        html = text_node_to_html_node(node)
+        self.assertEqual(html.tag, "b")
+        self.assertEqual(html.value, "Strong")
+        self.assertIsNone(html.props)
+
+    def test_italic(self):
+        node = TextNode("slant", TextType.ITALIC)
+        html = text_node_to_html_node(node)
+        self.assertEqual(html.tag, "i")
+        self.assertEqual(html.value, "slant")
+
+    def test_code(self):
+        node = TextNode("x = 1", TextType.CODE)
+        html = text_node_to_html_node(node)
+        self.assertEqual(html.tag, "code")
+        self.assertEqual(html.value, "x = 1")
+
+    def test_link(self):
+        node = TextNode("Boot.dev", TextType.LINK, "https://boot.dev")
+        html = text_node_to_html_node(node)
+        self.assertEqual(html.tag, "a")
+        self.assertEqual(html.value, "Boot.dev")
+        self.assertEqual(html.props, {"href": "https://boot.dev"})
+
+    def test_image(self):
+        node = TextNode("A bear", TextType.IMAGE, "https://img.com/bear.png")
+        html = text_node_to_html_node(node)
+        self.assertEqual(html.tag, "img")
+        self.assertEqual(html.value, "")
+        self.assertEqual(
+            html.props, {"src": "https://img.com/bear.png", "alt": "A bear"}
+        )
+
+    def test_unsupported_type_raises(self):
+        class FakeType:
+            pass
+
+        node = TextNode("x", FakeType())
+        with self.assertRaises(Exception):
+            text_node_to_html_node(node)
+
 
 if __name__ == "__main__":
     unittest.main()
