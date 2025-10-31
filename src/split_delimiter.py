e75578c42c22009608c3cf99c04504fd90d6b6be
Christopher-James Santiago <54423958+limitedink@users.noreply.github.com>
2025-10-31 18:46:38 +1100

    WIP: ADD basic structure to split_delimiter.py

--- src/split_delimiter.py
+++ src/split_delimiter.py
@@ -1,2 +1,6 @@
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    
+    new_nodes = []
+    if old_nodes.text_type != TextType.PLAIN:
+        new_nodes.append(old_nodes)
+
+    return new_nodes
