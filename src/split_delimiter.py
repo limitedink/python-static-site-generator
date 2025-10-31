c61e7da253130e22648c5f94ee9337c971f84111
limitedink <54423958+limitedink@users.noreply.github.com>
2025-10-31 21:21:57 +1100

    add delimiter detection logic to split_nodes_delimiter

--- src/split_delimiter.py
+++ src/split_delimiter.py
@@ -1,6 +1,29 @@
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
-    if old_nodes.text_type != TextType.PLAIN:
-        new_nodes.append(old_nodes)
+    for node in old_nodes:
+        if node.text_type != TextType.PLAIN:
+            new_nodes.append(node)
+        elif node.text_type == TextType.PLAIN:
+            text = node.text
+            delimiter = ""
+            for letter in text:
+                if letter == "`":
+                    delimiter = "`"
+                    parts = node.text.split(delimiter)
+                    if parts % 2 == 0:
+                        raise Exception("invalid Markdown syntax: missing delimiter")
+                    for part in parts:
+                        split_nodes_delimiter([part], delimiter, TextType.CODE)
+
+                elif letter == "**":
+                    delimiter = "**"
+                    split_nodes_delimiter([node], delimiter, TextType.BOLD)
+
+                elif letter == "_":
+                    delimiter = "_"
+                    split_nodes_delimiter([node], delimiter, TextType.ITALIC)
+
+                else:
+                    raise Exception("ERR:no delimiters found.")
 
     return new_nodes
