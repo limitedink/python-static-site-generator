77df3c518df93d269da3cd0c4e0679c4cf4017ef
Christopher-James Santiago <54423958+limitedink@users.noreply.github.com>
2025-11-02 15:39:22 +1100

    Add functions to extract markdown links and images
    Implement extract_markdown_links and extract_markdown_images in extract_links.py using regex. Add import and test for extract_markdown_links in test_extract_links.py.

--- src/test_extract_links.py
+++ src/test_extract_links.py
@@ -1,4 +1,5 @@
 import unittest
+from extract_links import extract_markdown_images, extract_markdown_links
 
 
 class TestExtractLinks(unittest.TestCase):
@@ -7,6 +8,12 @@ class TestExtractLinks(unittest.TestCase):
             "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
         )
         self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to google](https://google.com)"
+        )
+        self.assertListEqual([("to google", "https://google.com")], matches)
 
 
 if __name__ == "__main__":
