269e2720cda3f12f405df0644c793617566c00d8
Christopher-James Santiago <54423958+limitedink@users.noreply.github.com>
2025-11-04 09:18:46 +1100

    feat(split): add text_to_textnodes helper to convert text into parsed TextNodes
    Implements text_to_textnodes() to sequentially split input text into TextNodes
    for code, image, link, bold, and italic formatting using existing split helpers.

--- src/split.py
+++ src/split.py
@@ -89,3 +89,13 @@ def split_nodes_link(old_nodes):
             new_nodes.append(TextNode(text, TextType.PLAIN))
 
     return new_nodes
+
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.PLAIN)]
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    return nodes
