646a86b21a764a407bea1a724e3caf9f270ab06e
Christopher-James Santiago <54423958+limitedink@users.noreply.github.com>
2025-11-08 16:52:29 +1100

    Add basepath for GitHub Pages and update build process
    - Remove public/ from .gitignore and switch build to docs/
    - Add configurable basepath to handle subdirectory URLs
    - Update generate_page and generate_pages_recursive for basepath
    - Add new content (blog posts, contact page) and images
    - Create build.sh for production builds

--- src/gencontent.py
+++ src/gencontent.py
@@ -10,7 +10,7 @@ def extract_title(md):
     raise Exception("no h1 header found.")
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath="/"):
     print(
         f'Generating page from "{from_path}" to "{dest_path}" using "{template_path}"'
     )
@@ -24,9 +24,30 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(markdown)
     page = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
 
+    page = page.replace('href="/', f'href="{basepath}')
+    page = page.replace('src="/', f'src="{basepath}')
+
     dest_dir = os.path.dirname(dest_path)
     if dest_dir:
         os.makedirs(dest_dir, exist_ok=True)
 
     with open(dest_path, "w") as f:
         f.write(page)
+
+
+def generate_pages_recursive(
+    dir_path_content, template_path, dest_dir_path, basepath="/"
+):
+    print(
+        f'Generating pages from "{dir_path_content}" to "{dest_dir_path}" using "{template_path}"'
+    )
+
+    files = os.listdir(dir_path_content)
+    for name in files:
+        src_path = os.path.join(dir_path_content, name)
+        if os.path.isdir(src_path):
+            sub_dest = os.path.join(dest_dir_path, name)
+            generate_pages_recursive(src_path, template_path, sub_dest, basepath)
+        elif os.path.isfile(src_path) and name.endswith(".md"):
+            dest_path = os.path.join(dest_dir_path, os.path.splitext(name)[0] + ".html")
+            generate_page(src_path, template_path, dest_path, basepath)
--- src/main.py
+++ src/main.py
@@ -1,15 +1,16 @@
+import sys
 from shutil import copy
 from textnode import TextNode, TextType
 from copy_static import copy_static
-from gencontent import generate_page
+from gencontent import generate_page, generate_pages_recursive
 
 
 def main():
     # T_node = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
     # print(T_node)
-
-    copy_static("./static", "./public")
-    generate_page("content/index.md", "template.html", "public/index.html")
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
+    copy_static("./static", "./docs")
+    generate_pages_recursive("content", "template.html", basepath)
 
 
 if __name__ == "__main__":
